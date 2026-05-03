03cc69fb251a15b761064655c52c04d2204e1985
王坤平 <1970138194@qq.com>
2026-05-03 09:55:54 +0800

    fix: 打包后图标显示

--- app/components/PlatformLogo.vue
+++ app/components/PlatformLogo.vue
@@ -6,15 +6,15 @@ const props = defineProps<{
 }>();
 
 const logoMap: Record<string, string> = {
-  小红书: "/platform-icons/xiaohongshu.ico",
-  视频号: "/platform-icons/weixin-channels.ico",
-  抖音: "/platform-icons/douyin.ico",
-  快手: "/platform-icons/kuaishou.ico",
-  哔哩哔哩: "/platform-icons/bilibili.ico",
-  知乎: "/platform-icons/zhihu.ico",
-  百家号: "/platform-icons/baijiahao.ico",
-  搜狐号: "/platform-icons/sohu.ico",
-  今日头条: "/platform-icons/toutiao.ico",
+  小红书: "./platform-icons/xiaohongshu.ico",
+  视频号: "./platform-icons/weixin-channels.ico",
+  抖音: "./platform-icons/douyin.ico",
+  快手: "./platform-icons/kuaishou.ico",
+  哔哩哔哩: "./platform-icons/bilibili.ico",
+  知乎: "./platform-icons/zhihu.ico",
+  百家号: "./platform-icons/baijiahao.ico",
+  搜狐号: "./platform-icons/sohu.ico",
+  今日头条: "./platform-icons/toutiao.ico",
 };
 
 const src = computed(() => logoMap[props.platform] ?? "");
